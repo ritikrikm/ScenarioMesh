c506225866c68acef6ea31bcc478d6df0c313623
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-19 17:59:16 -0400

    Add Failsafe-selected integration test

---
+++ examples/failsafe-junit5-example/src/test/java/example/LoginIT.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class LoginIT {
+    @Test
+    void validLogin() {
+        assertTrue(true);
+    }
+}
