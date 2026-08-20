efb537468afb8204ed75d16e862b9d72c0a2b4c8
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-19 23:37:19 -0400

    Add cleanup hook tests

---
+++ examples/cleanup-hook-example/src/test/java/example/CleanupHookTest.java
@@ -0,0 +1,17 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CleanupHookTest {
+    @Test
+    void firstTask() {
+        assertTrue(true);
+    }
+
+    @Test
+    void secondTask() {
+        assertTrue(true);
+    }
+}
