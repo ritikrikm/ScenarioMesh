4f97509128d5e9d56139c986e2ca69bf3ee54275
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-20 19:49:27 -0400

    Configure native shared Cucumber JSON output for report compatibility fixture

--- examples/cucumber-junit4-example/src/test/java/example/RunCucumberTest.java
+++ examples/cucumber-junit4-example/src/test/java/example/RunCucumberTest.java
@@ -5,6 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features", glue = "example.steps")
+@CucumberOptions(
+        features = "classpath:features",
+        glue = "example.steps",
+        plugin = {"pretty", "json:target/cucumber-report/cucumber.json"})
 public class RunCucumberTest {
 }
