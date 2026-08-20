4e7ba44fc8dc6fcc25739b942b06db21907f643d
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-20 01:18:45 -0400

    test: add ordinary JUnit 4 ownership fixture

---
+++ scenariomesh-adapter-cucumber-junit4/src/test/java/io/scenariomesh/adapter/cucumberjunit4/HiddenLegacyTest.java
@@ -0,0 +1,10 @@
+package io.scenariomesh.adapter.cucumberjunit4;
+
+import org.junit.Test;
+
+public class HiddenLegacyTest {
+    @Test
+    public void ordinaryJUnit4Test() {
+        // Fixture used to prove Cucumber takeover cannot silently omit generic JUnit 4 work.
+    }
+}
