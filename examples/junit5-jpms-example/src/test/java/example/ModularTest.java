21ddb32fdc12e22eac5c2dc1c713347423a7e79a
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-26 22:09:26 -0400

    test(p1): add modular JUnit test

---
+++ examples/junit5-jpms-example/src/test/java/example/ModularTest.java
@@ -0,0 +1,9 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ModularTest {
+    @Test void modulePathContract() { assertEquals(4, 2 + 2); }
+}
