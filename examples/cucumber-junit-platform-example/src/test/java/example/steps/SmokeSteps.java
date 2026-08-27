7f9b59eebc4eec919756aa4fd02f89532c876c7e
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-26 20:26:46 -0400

    Prove Cucumber global lifecycle executes once

--- examples/cucumber-junit-platform-example/src/test/java/example/steps/SmokeSteps.java
+++ examples/cucumber-junit-platform-example/src/test/java/example/steps/SmokeSteps.java
@@ -1,8 +1,29 @@
 package example.steps;
 
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 public class SmokeSteps {
+    private static final Path LIFECYCLE_LOG = Path.of("target", "cucumber-lifecycle.log");
+
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+        Files.createDirectories(LIFECYCLE_LOG.getParent());
+        Files.writeString(LIFECYCLE_LOG, "BEFORE_ALL\n",
+                StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+    }
+
+    @AfterAll
+    public static void afterAll() throws Exception {
+        Files.writeString(LIFECYCLE_LOG, "AFTER_ALL\n",
+                StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+    }
+
     @Given("a short task")
     public void shortTask() throws Exception {
         Thread.sleep(150);
