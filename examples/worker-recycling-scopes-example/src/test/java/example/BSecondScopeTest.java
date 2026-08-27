acfbba1b3d20c2bb0772eb722762064bebdcb68f
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-26 20:55:11 -0400

    Add second independent recycling scope

---
+++ examples/worker-recycling-scopes-example/src/test/java/example/BSecondScopeTest.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BSecondScopeTest {
+    @Test
+    void secondScope() {
+        assertTrue(true);
+    }
+}
