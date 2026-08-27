7fd7037b4754b434263bc5e0a7d175f85ae81ba2
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-26 20:54:59 -0400

    Add first independent recycling scope

---
+++ examples/worker-recycling-scopes-example/src/test/java/example/AFirstScopeTest.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AFirstScopeTest {
+    @Test
+    void firstScope() {
+        assertTrue(true);
+    }
+}
