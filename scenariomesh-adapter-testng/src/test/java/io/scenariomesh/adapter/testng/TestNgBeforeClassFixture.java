69200f06d852d9cdb5b82dfca8f5ead11573ab48
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-29 08:16:48 -0400

    Add TestNG class-lifecycle fixture

---
+++ scenariomesh-adapter-testng/src/test/java/io/scenariomesh/adapter/testng/TestNgBeforeClassFixture.java
@@ -0,0 +1,9 @@
+package io.scenariomesh.adapter.testng;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestNgBeforeClassFixture {
+    @BeforeClass public void setupClass() { }
+    @Test public void test() { }
+}
