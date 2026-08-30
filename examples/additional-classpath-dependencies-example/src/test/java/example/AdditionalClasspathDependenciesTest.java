085b2b3717e5da0fca42116dbbf24dc5fdaa7113
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-29 20:24:43 -0400

    test(maven): assert effective additional dependency realm

--- examples/additional-classpath-dependencies-example/src/test/java/example/AdditionalClasspathDependenciesTest.java
+++ examples/additional-classpath-dependencies-example/src/test/java/example/AdditionalClasspathDependenciesTest.java
@@ -2,17 +2,28 @@ package example;
 
 import org.junit.jupiter.api.Test;
 
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AdditionalClasspathDependenciesTest {
     @Test
     void additionalDependencyAndItsTransitiveAreVisibleWithoutProjectConflictMediation() throws Exception {
-        Class.forName("org.apache.commons.text.StringSubstitutor");
-        Class.forName("org.apache.commons.lang3.StringUtils");
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Class.forName("org.apache.commons.text.StringSubstitutor", true, loader);
+        Class.forName("org.apache.commons.lang3.StringUtils", true, loader);
 
-        String classpath = System.getProperty("java.class.path", "");
-        assertTrue(classpath.contains("commons-lang3-3.12.0.jar"), classpath);
-        assertTrue(classpath.contains("commons-lang3-3.14.0.jar"), classpath);
-        assertTrue(classpath.contains("commons-text-1.12.0.jar"), classpath);
+        List<String> langMetadata = Collections.list(
+                        loader.getResources("META-INF/maven/org.apache.commons/commons-lang3/pom.properties"))
+                .stream().map(URL::toString).toList();
+        List<String> textMetadata = Collections.list(
+                        loader.getResources("META-INF/maven/org.apache.commons/commons-text/pom.properties"))
+                .stream().map(URL::toString).toList();
+
+        assertTrue(langMetadata.stream().anyMatch(value -> value.contains("commons-lang3-3.12.0.jar")), langMetadata.toString());
+        assertTrue(langMetadata.stream().anyMatch(value -> value.contains("commons-lang3-3.14.0.jar")), langMetadata.toString());
+        assertTrue(textMetadata.stream().anyMatch(value -> value.contains("commons-text-1.12.0.jar")), textMetadata.toString());
     }
 }
