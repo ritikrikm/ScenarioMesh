a53d8f24aacf44e5159f80beb53f6fc4389e0550
ritikrik <109413784+ritikrikm@users.noreply.github.com>
2026-08-31 00:22:21 -0400

    test(testng): add top-level advanced semantics fixtures

---
+++ scenariomesh-adapter-testng/src/test/java/io/scenariomesh/adapter/testng/TestNgClassLevelFixture.java
@@ -0,0 +1,9 @@
+package io.scenariomesh.adapter.testng;
+
+import org.testng.annotations.Test;
+
+@Test
+public final class TestNgClassLevelFixture {
+    public void first() { }
+    public void second() { }
+}
